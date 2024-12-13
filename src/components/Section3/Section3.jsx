aa045ec21060148317b533b58175dc16d6b4d6f3
otabekabduhalilov2010 <otabekabduhalilov80@gmail.com>
2024-12-13 15:25:08 +0500

    Add files via upload

---
+++ src/components/Section3/Section3.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import './Section3.scss'
+const Section3 = () => {
+  return (
+
+    <>
+    
+    <section className="section3">
+        <div className="container">
+                <h1 className='text'>OUR HAPPY CUSTOMERS</h1>
+            <div className="wrapper4">
+                <div className="box5">
+                    ⭐⭐⭐⭐⭐
+                   <h4> Salah.M✅</h4>
+                    <p>"I'm blown away by the quality and style of the clothes I received from Shop.co. <br /> From casual wear to elegant dresses, every piece I've bought has exceeded my expectations.”</p>
+                </div>
+                <div className="box5">
+                    ⭐⭐⭐⭐⭐
+                   <h4> Salah.M✅</h4>
+                    <p>"I'm blown away by the quality and style of the clothes I received from Shop.co. <br /> From casual wear to elegant dresses, every piece I've bought has exceeded my expectations.”</p>
+                </div>
+                <div className="box5">
+                    ⭐⭐⭐⭐⭐
+                   <h4> Salah.M✅</h4>
+                    <p>"I'm blown away by the quality and style of the clothes I received from Shop.co. <br /> From casual wear to elegant dresses, every piece I've bought has exceeded my expectations.”</p>
+                </div>
+            
+            </div>
+
+            <div className="wrapper5">
+                <h1>STAY UPTO DATE ABOUT OUR <br /> LATEST OFFERS</h1>
+                    <input placeholder='Enter your email' type="text" />
+            </div>
+        </div>
+    </section>
+    
+    </>
+   
+  )
+}
+
+export default Section3
